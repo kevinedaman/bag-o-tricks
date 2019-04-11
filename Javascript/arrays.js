4079fd5a672079a53e289a4ebbfd280750d2895d
Kevin Dahlberg <kevin.dahlberg@cloudburststudio.com>
2019-04-11 01:33:59 -0500

    added sort array by 2 conditions

--- Javascript/arrays.js
+++ Javascript/arrays.js
@@ -2,3 +2,33 @@
  * creates array of numbers from a number.
  */
 Array.from(Array(length).keys());
+
+/**
+ * sort array alphabetically
+ */
+objArray.sort(function(a, b) {
+    var textA = a.DepartmentName.toUpperCase();
+    var textB = b.DepartmentName.toUpperCase();
+    return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+});
+
+/**
+ * sort array by 2 conditions
+ */
+objArray.sort((a, b) => {
+    if (a.c1 < b.c1) {
+        return -1;
+    }
+    if (a.c1 > b.c1) {
+        return 1;
+    }
+    if (a.c1 === b.c1) {
+        if (a.c2 < b.c2) {
+            return -1;
+        }
+        if (a.c2 > b.c2) {
+            return 1;
+        }
+    }
+    return 0
+});
